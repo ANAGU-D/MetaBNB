083d46d63e5a2f9f2f5410ec83d9764a52edd1e9
unknown <anagudaniel@gmail.com>
2022-11-15 17:10:12 -1100

    'doc:new'

---
+++ src/pages/home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+import Hero from '../components/hero'
+import Logo from '../components/logo'
+import Card from '../components/card'
+import Nfts from '../components/nfts'
+import Footer from '../components/footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Logo/>
+      <Card/>
+      <Nfts/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/placetostay.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
+import Places from '../components/places'
+
+
+
+const PlaceToStay = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Places/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default PlaceToStay
