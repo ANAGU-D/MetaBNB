9ed3250ec5e9c81589e62608e3cad2173a07e88c
unknown <anagudaniel@gmail.com>
2022-11-15 16:17:07 -1100

    'feat'

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,23 +9,7 @@ import copyright from '../images/copyright.svg';
 function Footer() {
   return (
     <div>
-      <div className='Footer-Container' class='flex flex-col h-[30%] w-[100%] bg-[black ]'>
-        <div className='footer-left'>
-            <img src={metalogo} alt='metlogo'/>
-            <div clss='flex flex-row'>
-                <img src={facebook} alt='facebook'/>
-                <img src={instagram} alt='instgrm'/>
-                <img src={twitter} alt='twitter'/>
-            </div>
-            <div>
-            <img src={copyright} alt='copyright'/>
-            </div>
-        </div>
-
-        <div className='Footer-right'>
-        <img src={footerr} alt='footer'/>
-        </div>
-      </div>
+      
     </div>
   )
 }
